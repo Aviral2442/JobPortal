9086447312a834c0dd1115402c777d5b95cf4830
Aviral2442 <aviralsrivastava24y15@gmail.com>
2025-12-29 10:21:04 +0530

    Update CORS origin to use the correct job portal URL

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174','https://job-portal-react-v2.vercel.app'], 
+  origin: ['http://localhost:5173', 'http://localhost:5174','https://jobportal-cloud.vercel.app'], 
   credentials: true,
 }));
 
