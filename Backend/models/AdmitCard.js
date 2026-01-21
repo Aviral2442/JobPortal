1f7b4d418781a441b495018c0c1abb4ff97701cb
Aviral2442 <aviralsrivastava24y15@gmail.com>
2026-01-21 12:48:26 +0530

    feat: add admitCard_ReleaseDate field to AdmitCard schema

--- Backend/models/AdmitCard.js
+++ Backend/models/AdmitCard.js
@@ -7,6 +7,7 @@ const AdmitCardSchema = new mongoose.Schema({
     admitCard_Desc: { type: String, required: true },
     admitCard_FilePath: { type: String, required: true },
     admitCard_URL: { type: String, required: false },
+    admitCard_ReleaseDate: { type: Number, required: true },
     admitCard_Status: { type: String, enum: ['active', 'inactive'], default: 'active' },
     admitCard_CreatedAt: { type: Number, default: () => currentUnixTime.currentUnixTimeStamp() },
     admitCard_UpdatedAt: { type: Number, default: () => currentUnixTime.currentUnixTimeStamp() }
