1061ecd19c1f59e47967be45c8a8da5ed2c39200
Aviral2442 <aviralsrivastava24y15@gmail.com>
2026-01-22 12:37:21 +0530

    Refactor code structure for improved readability and maintainability

--- Backend/models/student/studentBasicDetailModel.js
+++ Backend/models/student/studentBasicDetailModel.js
@@ -1,25 +1,26 @@
 // models/StudentBasicInfo.js
 const mongoose = require("mongoose");
+const currentUnixTime = require("../.././utils/currentUnixTimeStamp");
 
 const StudentBasicInfoSchema = new mongoose.Schema({
   studentId: { type: mongoose.Schema.Types.ObjectId, ref: "Student", required: true, index: true },
 
   studentDOB: { type: String, required: true },
-  studentGender: { type: String, required: true, enum: ["male","female","other","prefer_not_to_say"], default: "prefer_not_to_say" },
+  studentGender: { type: String, required: true, enum: ["male", "female", "other", "prefer_not_to_say"], default: "prefer_not_to_say" },
 
   studentAlternateMobileNo: { type: String, default: null },
 
-  studentMaritalStatus: { type: String, required: true, enum: ["single","married","other","prefer_not_to_say"], default: "single" },
+  studentMaritalStatus: { type: String, required: true, enum: ["single", "married", "other", "prefer_not_to_say"], default: "single" },
   studentMotherTongue: { type: String, default: null },
   studentNationality: { type: String, default: null },
   studentCitizenship: { type: String, default: null },
 
-  studentCreatedAt: { type: Number, default: () => Date.now() },
-  studentUpdatedAt: { type: Number, default: () => Date.now() }
+  studentCreatedAt: { type: Number, default: () => currentUnixTime.currentUnixTimeStamp() },
+  studentUpdatedAt: { type: Number, default: () => currentUnixTime.currentUnixTimeStamp() }
 });
 
-StudentBasicInfoSchema.pre("save", function(next){
-  this.studentUpdatedAt = Date.now();
+StudentBasicInfoSchema.pre("save", function (next) {
+  this.studentUpdatedAt = currentUnixTime.currentUnixTimeStamp();
   next();
 });
 
