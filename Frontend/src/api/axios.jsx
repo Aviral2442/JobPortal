fd398379d3a422618629c74df67dd0306da344cd
anis5165 <anishkumargvm02@gmail.com>
2026-03-12 12:57:08 +0530

    fix: update axios instance configuration for API base URL

--- Frontend/src/api/axios.jsx
+++ Frontend/src/api/axios.jsx
@@ -1,17 +1,17 @@
 import axios from "axios";
 
-const api = axios.create({
-  // baseURL: "https://jobportal-react-one.onrender.com/api",
-  baseURL: import.meta.env.VITE_BASE_URL + "/api",
-  withCredentials: true,
-});
-
-
-
 // const api = axios.create({
-//     baseURL: "/api",
-//     withCredentials: true,
-//   });
+//   // baseURL: "https://jobportal-react-one.onrender.com/api",
+//   baseURL: import.meta.env.VITE_BASE_URL + "/api",
+//   withCredentials: true,
+// });
+
+
+
+const api = axios.create({
+    baseURL: "/api",
+    withCredentials: true,
+  });
 
 
 
